5594967b686a13e1ea53674758bd6a0715a386ba
Apiiyu <khoirulloh.rafi2@gmail.com>
2023-11-21 18:02:44 +0700

    perf: added robots.txt file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,6 +17,7 @@
 	<meta name="author" content="Apiiyu" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="theme-color" content="#05051E" />
+	<meta name="robots" content="index, follow" />
 	<meta name="googlebot" content="index, follow" />
 	<meta name="google" content="notranslate" />
 	<meta name="google" content="nositelinkssearchbox" />
